b6008bf6d32fbe993d5439cd3601b88eacf5f76c
HARSH RAJ <119205864+HarshGodxpro@users.noreply.github.com>
2023-01-29 16:10:32 +0530

    Update audiobook.py

--- plugins/helper/audiobook.py
+++ plugins/helper/audiobook.py
@@ -40,7 +40,7 @@ async def pdf_to_text(bot, message):
                 tts_file.save(f"{message.chat.id}.mp3")      
                 with open(f"{message.chat.id}.mp3", "rb") as speech:
                       await bot.send_voice(chat_id, speech)   
-                await txt.edit("𝚃𝙷𝙰𝙽𝙺𝚂 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶 𝙼𝙴...☺️\n©𝐌𝐎𝐕𝐈𝐄𝐋𝐀𝐍𝐃.𝐂𝐎𝐌")    
+                await txt.edit("𝚃𝙷𝙰𝙽𝙺𝚂 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶 𝙼𝙴...☺️\n©ᴍᴏᴠɪᴇʟᴀɴᴅ.ᴄᴏᴍ")    
                 os.remove(pdf_path)  
                 
                 
