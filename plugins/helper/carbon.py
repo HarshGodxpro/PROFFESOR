b668e996432d0f27f3473159eaa6c34c90428d56
HARSH RAJ <119205864+HarshGodxpro@users.noreply.github.com>
2023-01-29 16:08:57 +0530

    Update carbon.py

--- plugins/helper/carbon.py
+++ plugins/helper/carbon.py
@@ -43,7 +43,7 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴛʜɪs ᴘɪᴄ ɪs ɴɪᴄᴇ ᴏɴᴇ\nᴍᴀᴅᴇ ʙʏ ᴄɪɴᴇᴍᴀʟᴀ.ᴄᴏᴍ**",
+        caption="**ᴛʜɪs ᴘɪᴄ ɪs ɴɪᴄᴇ ᴏɴᴇ\nᴍᴀᴅᴇ ʙʏ 𝐌𝐎𝐕𝐈𝐄𝐋𝐀𝐍𝐃.𝐂𝐎𝐌**",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=S_GROUP)]]),                   
     )
     await m.delete()
