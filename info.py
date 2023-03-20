404dd0b292067304c2aee8cb747bb08d33b00763
HARSH RAJ <119205864+HarshGodxpro@users.noreply.github.com>
2023-03-20 16:41:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -84,8 +84,8 @@ MONGO_URL = os.environ.get('MONGO_URL', "")
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 
 #url links
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shorturllink.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '3a3935e37c74a2384f7a689c414f078ab6320785')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'nanolinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '8534dd7c47097e1b64edc53831916c2b8af59496')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 
 # Others
